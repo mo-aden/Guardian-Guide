930cf811feacb1fec0674bbb1913e129ec954efd
rsheridan613 <rsheridan613@gmail.com>
2022-12-12 12:04:54 -0700

    added connection.js, server.js. created login route, User model

---
+++ controllers/home-routes.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+
+router.get("/login", (req, res) => {
+  // If the user is already logged in, redirect to the homepage
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+  // Otherwise, render the 'login' template
+  res.render("login");
+});
+
+module.exports = router;
