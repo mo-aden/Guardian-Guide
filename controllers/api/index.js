740592ec53f895c0cb5d946b89b3606a5a043317
rsheridan613 <rsheridan613@gmail.com>
2022-12-13 12:51:17 -0700

    added login routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const userRoutes = require("./dashboard.js");
 
+// .com/api
 router.use("/dashboard", userRoutes);
 
 module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,17 +4,17 @@ const { User } = require("../models");
 // .com/
 // If logged in
 router.get("/", (req, res) => {
-  // If the user is already logged in, redirect to the household dashboard
+  // If the user is logged in, redirect to the household dashboard
   if (req.session.loggedIn) {
     res.redirect("/dashboard");
     return;
   }
-  // Otherwise, render the 'homepage' template
+  // Stay on page if not
   res.render("/");
 });
 
 // When logging in
-router.post("/", async (req, res) => {
+router.post("/login", async (req, res) => {
   try {
     // Find email
     const dbUserData = await User.findOne({
@@ -22,24 +22,21 @@ router.post("/", async (req, res) => {
         email: req.body.email,
       },
     });
-
     if (!dbUserData) {
       res
         .status(400)
         .json({ message: "Incorrect email or password. Please try again!" });
       return;
     }
-
+    // Check password on row containing email
     const validPassword = await dbUserData.checkPassword(req.body.password);
-
     if (!validPassword) {
       res
         .status(400)
         .json({ message: "Incorrect email or password. Please try again!" });
       return;
     }
-
-    // Once the user successfully logs in, set up the sessions variable 'loggedIn'
+    // Give user set "loggedIn" to true
     req.session.save(() => {
       req.session.loggedIn = true;
 
@@ -47,6 +44,15 @@ router.post("/", async (req, res) => {
         .status(200)
         .json({ user: dbUserData, message: "You are now logged in!" });
     });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// When making a new account
+router.post("/signup", async (req, res) => {
+  try {
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
