1c4390791e056c54a3f96691654d8e8cd94a1be0
rsheridan613 <rsheridan613@gmail.com>
2022-12-13 13:54:49 -0700

    finished login routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
-const userRoutes = require("./dashboard.js");
+const dashboardRoutes = require("./dashboard-routes.js");
+const userRoutes = require("./user-routes");
 
 // .com/api
-router.use("/dashboard", userRoutes);
+router.use("/dashboard", dashboardRoutes);
+router.use("/users", userRoutes);
 
 module.exports = router;
---
+++ controllers/api/user-routes.js
@@ -0,0 +1,74 @@
+const router = require("express").Router();
+const { User } = require("../../models");
+
+// .com/api/dashboard
+// When logging in
+router.post("/login", async (req, res) => {
+  try {
+    // Find email
+    const dbUserData = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+    if (!dbUserData) {
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password. Please try again!" });
+      return;
+    }
+    // Check password on row containing email
+    const validPassword = await dbUserData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password. Please try again!" });
+      return;
+    }
+    // Give user set "loggedIn" to true
+    // req.session.save(() => {
+    //   req.session.loggedIn = true;
+
+    res
+      .status(200)
+      .json({ user: dbUserData, message: "You are now logged in!" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+//When logging out
+router.post("/logout", (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+// When making a new account
+router.post("/", async (req, res) => {
+  try {
+    // Create new user
+    const dbUserData = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    // Add 'loggedIn' variable set to `true`
+    req.session.save(() => {
+      req.session.loggedIn = true;
+
+      res.status(200).json(dbUserData);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
