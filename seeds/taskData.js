6b365c2a5aed3b224bf7491198623deacff7c04f
Ryan <rylarson17@gmail.com>
2022-12-13 10:33:31 -0700

    Added to the tasklist in the seed data

--- seeds/taskData.js
+++ seeds/taskData.js
@@ -22,6 +22,48 @@ const taskData = [
     due_date: "2022-12-13 10:00:00",
     family_id: 3,
   },
+  {
+    name: "Dentist Appointment",
+    category: "Appointment",
+    description: "Teeth cleaning appointment",
+    due_date: "2022-12-13 09:00:00",
+    family_id: 4,
+  },
+  {
+    name: "Basketball Game",
+    category: "Event",
+    description: "Youth basketball game",
+    due_date: "2022-12-13 08:00:00",
+    family_id: 5,
+  },
+  {
+    name: "Take out trash",
+    category: "Chore",
+    description: "Take out the trash for pickup",
+    due_date: "2022-12-13 09:00:00",
+    family_id: 6,
+  },
+  {
+    name: "Homework",
+    category: "To-do",
+    description: "Complete math homework",
+    due_date: "2022-12-13 09:00:00",
+    family_id: 7,
+  },
+  {
+    name: "Pay rent",
+    category: "To-do",
+    description: "Submit monthly payment for rent",
+    due_date: "2022-12-13 09:00:00",
+    family_id: 8,
+  },
+  {
+    name: "School Play",
+    category: "Event",
+    description: "Middle School winter play",
+    due_date: "2022-12-13 09:00:00",
+    family_id: 9,
+  },
 ];
 
 const seedTask = () => Task.bulkCreate(taskData);
