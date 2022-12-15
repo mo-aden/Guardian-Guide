bc9d03db7a441227f5f82d2a56000d9ecbc58852
rsheridan613 <rsheridan613@gmail.com>
2022-12-15 10:14:55 -0700

    created routes to delete family members/tasks

--- controllers/api/family-routes.js
+++ controllers/api/family-routes.js
@@ -6,6 +6,23 @@ const { User, Family, Task } = require("../../models");
 router.post("/", async (req, res) => {});
 
 // Delete a family member
-router.delete("/:id", async (req, res) => {});
+router.delete("/:id", async (req, res) => {
+  try {
+    const familyData = await Family.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!familyData) {
+      res.status(404).json({ message: "No family member with is id" });
+      return;
+    }
+
+    res.status(200).json(familyData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
--- controllers/api/task-routes.js
+++ controllers/api/task-routes.js
@@ -6,6 +6,23 @@ const { User, Family, Task } = require("../../models");
 router.post("/", async (req, res) => {});
 
 // Delete a task
-router.delete("/:id", async (req, res) => {});
+router.delete("/:id", async (req, res) => {
+  try {
+    const taskData = await Task.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!taskData) {
+      res.status(404).json({ message: "No task with is id" });
+      return;
+    }
+
+    res.status(200).json(taskData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
