64d151cdb5785a1da15dd4113c2a405cd2a63bda
Ryan <rylarson17@gmail.com>
2022-12-15 20:33:35 -0700

    Logout redirects to homepage

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -12,38 +12,19 @@ $(document).ready(function () {
 });
 
 // Logout link
-const logoutHandler = async function () {
-  const response = await fetch("/api/user/logout", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-  });
+const logoutHandler = async function (event) {
+  event.preventDefault();
 
-  if (response.ok) {
-    document.location.replace("/");
-  } else {
-    alert("Failed to log out");
-  }
+  // const response = await fetch("/api/users/logout", {
+  //   method: "POST",
+  //   headers: { "Content-Type": "application/json" },
+  // });
+
+  // if (response.ok) {
+  document.location.replace("../");
+  // } else {
+  //   alert("Failed to log out");
+  // }
 };
 
-const sendToNewTask = async () => {
-  const idResponse = await fetch("/api/users", {
-    method: "GET",
-  });
-
-  const data = await idResponse.json();
-
-  const index = data.findIndex((el) => {
-    if (el.username === usernameEl.value) return true;
-  });
-
-  const id = data[index].id;
-
-  if (idResponse.ok) {
-    document.location.replace(`/dashboard/${id}/new`);
-  } else {
-    alert("Failed to sign up");
-  }
-};
-
-document.querySelector("#create-button").addEventListener("click", newTaskPage);
 document.querySelector("#logout-link").addEventListener("click", logoutHandler);
